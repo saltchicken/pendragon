585f001b08dd565832bd8d2867cbb7a992b925bf
saltchicken <john.eicher89@gmail.com>
2026-07-10 14:29:10 -0700

    Use default for image_path so it shows up in settings

--- src/pendragon/plugins/generators/other/photo_stipple.py
+++ src/pendragon/plugins/generators/other/photo_stipple.py
@@ -18,7 +18,10 @@ class PhotoStippleConfig(BasePluginConfig):
     dots: int = Field(default=5000,
                       gt=0,
                       description="Target number of stipple dots to generate.")
-    image_path: str | None = Field(default=None, description="Source image.")
+    image_path: str = Field(
+        default="", 
+        description="Source image."
+    )
     dot_size: float = Field(default=0.1,
                             gt=0.0,
                             description="Length of microscopic line.")
